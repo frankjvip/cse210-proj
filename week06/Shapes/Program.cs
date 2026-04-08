0ccb8c7fe2f78f5e576e37612f132727765cfbc9
Franklin Rivas <frivas@byupathway.edu>
2026-04-07 23:31:38 -0600

    finished shapes exercise

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,22 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        // Crear lista de figuras
+        List<Shape> shapes = new List<Shape>();
+
+        // Agregar diferentes figuras
+        shapes.Add(new Square("Red", 5));
+        shapes.Add(new Rectangle("Blue", 4, 6));
+        shapes.Add(new Circle("Green", 3));
+
+        // Iterar y mostrar resultados
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Color: {shape.GetColor()}, Area: {shape.GetArea()}");
+        }
     }
 }
