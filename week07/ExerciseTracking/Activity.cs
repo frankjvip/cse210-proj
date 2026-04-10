bae663472532093e5aeeaa1b7f6d689c4ca26268
Franklin Rivas <frivas@byupathway.edu>
2026-04-09 23:01:13 -0600

    added the screenshot

---
+++ week07/ExerciseTracking/Activity.cs
@@ -0,0 +1,34 @@
+using System;
+
+public abstract class Activity
+{
+    private DateTime _date;
+    private int _minutes;
+
+    public Activity(DateTime date, int minutes)
+    {
+        _date = date;
+        _minutes = minutes;
+    }
+
+    public DateTime GetDate()
+    {
+        return _date;
+    }
+
+    public int GetMinutes()
+    {
+        return _minutes;
+    }
+
+    // Abstract methods to be overridden
+    public abstract double GetDistance();
+    public abstract double GetSpeed();
+    public abstract double GetPace();
+
+    public virtual string GetSummary()
+    {
+        return $"{_date.ToString("dd MMM yyyy")} {this.GetType().Name} ({_minutes} min) - " +
+               $"Distance {GetDistance():0.0} km, Speed {GetSpeed():0.0} kph, Pace: {GetPace():0.00} min per km";
+    }
+}
--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -1,9 +1,21 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        List<Activity> activities = new List<Activity>();
+
+        // Create one of each activity
+        activities.Add(new Running(new DateTime(2022, 11, 3), 30, 4.8)); // 4.8 km in 30 min
+        activities.Add(new Cycling(new DateTime(2022, 11, 3), 40, 20.0)); // 20 kph for 40 min
+        activities.Add(new Swimming(new DateTime(2022, 11, 3), 25, 30)); // 30 laps in 25 min
+
+        // Display summaries
+        foreach (Activity activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
